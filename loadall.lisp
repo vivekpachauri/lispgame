9c9a02433a7d95fc0011c1db3bb6ce3b961f2780
vpachauri <devraj0109@gmail.com>
2014-04-02 19:03:52 -0400

    trying to restructure the folder

--- loadall.lisp
+++
@@ -1,6 +0,0 @@
-;;;in this file I will keep the basic code to load all the other files,
-;;;this way I can just load one file without having to load files separately
-(load "orcbattle.lisp")
-(load "wumpus.lisp")
-(load "lispgame.lisp")
-(load "evolution.lisp")
